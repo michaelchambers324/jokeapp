e4bb961b1b41f0a47a4bc5f6e0bedf6f3c4e8e69
Michael Chambers <michael.chambers@nextgearcapital.com>
2018-08-30 22:46:59 -0400

    Initial Commit

---
+++ src/main/java/com/project/joke/jokeapp/services/JokeService.java
@@ -0,0 +1,5 @@
+package com.project.joke.jokeapp.services;
+
+public interface JokeService {
+    String getJoke();
+}
---
+++ src/main/java/com/project/joke/jokeapp/services/JokeServiceImpl.java
@@ -0,0 +1,18 @@
+package com.project.joke.jokeapp.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JokeServiceImpl implements JokeService {
+
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public JokeServiceImpl(ChuckNorrisQuotes chuckNorrisQuotes) {
+        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    }
+
+    @Override
+    public String getJoke() {
+        return chuckNorrisQuotes.getRandomQuote();
+    }
+}
